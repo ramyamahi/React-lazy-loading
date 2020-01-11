7978a26b93bdb2f9e1a07d2e729c68190ef39012
ramya <ramya.m@spritle.com>
2020-01-11 16:31:57 +0530

    Initial commit

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+	render () {
+  	return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
+
+export default Main
